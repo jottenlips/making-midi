1c2abcc6b812eaaeb52abc257a5a15771139141a
John Ottenlips <johno@aloompa.com>
2018-11-18 21:13:54 -0600

    basic chord progression with tonal and tone js

--- src/App.js
+++ src/App.js
@@ -3,43 +3,78 @@ import logo from './logo.svg';
 import './App.css';
 // import midi from 'midi';
 import Tone  from 'tone';
-import { Scale, Chord } from "tonal";
+import { Chord } from "tonal";
+// import Math
 
-// var now = Transport.now()
-// // Transport.bpm.setValueAtTime(100, now);
-// Transport.bpm.setValueAtTime(150, now);
-// Transport.bpm.timeToTicks(6, now).toBarsBeatsSixteenths()
+const chordForOctave = (chord, octave) => {
+  return chord.map(note => `${note}${octave}`);
+}
+const synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
 
-var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+const song = [{notes:Chord.notes("Bbm"), octave:4, measureLength:2, rate:4, index:0}, 
+{notes:Chord.notes("BbM"), octave:4, measureLength:4, rate:4}, 
+{notes:Chord.notes("BbM"), octave:4, measureLength:4, rate:4},
+{notes:Chord.notes("AM"), octave:4, measureLength:4, rate:4},
+{notes:Chord.notes("Am"), octave:4, measureLength:4, rate:4},
+{notes:Chord.notes("BbM"), octave:4, measureLength:4, rate:4}];
 
+const parseSong = (song) => {
+  const lengths = measureLengths(song);
+  console.log(lengths)
+  const markers = measureMarkers(lengths);
+  console.log('markers', markers)
+  generateChordSequence(song, markers);
+  Tone.Transport.start();
+}
 
-console.log(Chord.notes("BbMaj"))
-const chordNotes = Chord.notes("Bbm").map(note => `${note}4`);
-const time = "4n";
+const measureLengths = (song) => (song.map((accumulator) => {
+    return parseInt(accumulator.measureLength);
+    })
+);
 
-var chord = new Tone.Event(function(time, chord){
-	//the chord as well as the exact time of the event
-  //are passed in as arguments to the callback function
-      // synth.triggerAttackRelease(chord, "4n");
-}, chordNotes);
-//start the chord at the beginning of the transport timeline
-chord.start();
-//loop it every measure for 8 measures
-chord.loop = 8;
-chord.loopEnd = "1m";
+const measureMarkers = (measureLengths) => [0,
+  ...(measureLengths.reduce((arr, current, index) =>
+    (arr.push((index && arr[index - 1] || 0) + current), arr), []))];
 
-// var seq = new Tone.Sequence( (time, note) => {
-//     synth.triggerAttackRelease(note, "8n", time);
-// }, songObj, time);
+const generateChordSequence = (song, markers) => {
+  song.map((chord, index) => {
+    chord.notes.map(note => {
+      const event = new Tone.Event(function(time, pitch) {
+        console.log(note)
+        synth.triggerAttackRelease(pitch, "4n", time);
+      }, `${note}4`)
+      event.start(`${markers[index]}m`);
+      event.stop(`${markers[index]+chord.measureLength}m`);
+      event.set({
+        "loop" : true,
+        "loopEnd" : "4n"
+      });
+    })
+  });
+}
 
-// const progressionGenerator()
+parseSong(song);
 
+// const notes = Chord.notes("Bbm");
+// const chordNotes = chordForOctave(notes, '4');
 
-// seq.start(0);
-// seq2.start(0);
-// seq3.start(0);
-Tone.Transport.start();
+// const notes2 = Chord.notes("BbM");
+// const chordNotes2 = chordForOctave(notes2, '4');
 
+// const length = "4m";
+// Tone.Transport.bpm.value = 100
+// const rate = '4n'
+// var loop = new Tone.Loop(function(time){
+// 	//triggered every eighth note. 
+//   // synth.triggerAttackRelease(chordNotes, rate);
+// }, rate).start(0);
+// loop.stop(length);
+
+// var loop2 = new Tone.Loop(function(time){
+// 	//triggered every eighth note. 
+//   // synth.triggerAttackRelease(chordNotes2, rate);
+// }, rate).start('4m');
+// loop2.stop('8m');
 
 class App extends Component {
   render() {
@@ -57,7 +92,6 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
         </header>
       </div>
