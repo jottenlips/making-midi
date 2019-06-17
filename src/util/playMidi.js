a39cba5e56a674482ec6bfbbca0cb38b6163230b
John Ottenlips <johno@aloompa.com>
2019-06-16 19:22:02 -0500

    no more tone js

--- src/util/playMidi.js
+++ src/util/playMidi.js
@@ -1,24 +1,20 @@
-
-import * as Tone from 'tone';
-import Midi from '@tonejs/midi'
-
+const MidiPlayer = require('midi-player-js');
+const Soundfont = require('sounfont-player');
+var ac = new AudioContext()
 
 export const playMidi = async (dataUri) => {
-    const midi = await Midi.fromUrl(dataUri)
-    console.log(midi)
-    const now = Tone.now() + 4
-
-    return midi.tracks.map(track => {
-        var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
-        return Promise.all(track.notes.map(note => {
-            console.log(note.name, note.time)
-            return synth.triggerAttackRelease(note.name, note.duration, note.time + now)
-        }))
-        // return synth.triggerAttackRelease(track.notes, "4n");
-
-    
-    })
-    
+   
+    var Player = new MidiPlayer.Player(function(event) {
+        Soundfont.instrument(ac, 
+            'https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/MusyngKite/acoustic_guitar_nylon-mp3.js')
+                .then((instrument) => {
+            if (event.name == 'Note on') {
+                instrument.play(event.noteName, ac.currentTime, {gain:event.velocity/100});
+            }
+        })
+    });
+    Player.loadDataUri(dataUri);
+    Player.play();
 }
 
-//   Tone.Transport.start();
+
