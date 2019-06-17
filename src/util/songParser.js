b921fe4de95a8d103a77b8aa2fec90a363d84379
John Ottenlips <johno@aloompa.com>
2019-06-16 20:15:05 -0500

    spreading octave nicely

--- src/util/songParser.js
+++ src/util/songParser.js
@@ -13,7 +13,7 @@ const parseChords = (measures) => {
         .map(measure => measure.filter(chord => chord.length > 0));
 }
 
-const spreadOctave = (index) => index % 2 === 0 ? index -1 : 0
+const spreadOctave = (index) => index % 2 === 0 ? 1 : 0
 
 const appendProgressionDetails = (chords, octave) => {
     return chords.map(measure => measure.map(chord => ({
