d35be7cd1f55334be8056d7c7922ce5d0bd6b975
John Ottenlips <jottenlips@gmail.com>
2020-02-14 19:10:23 -0600

    fix chord input

--- src/components/ChordInput.js
+++ src/components/ChordInput.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { parseSong } from '../util/songParser';
-import { compose, withState } from 'recompose';
-import './chordInput.css';
-import { composeMidiFile } from '../util/composeMidiFile';
-import { flatten } from 'ramda';
-import { playMidi } from '../util/playMidi';
+import React, { useState } from "react";
+import { parseSong } from "../util/songParser";
+import "./chordInput.css";
+import { composeMidiFile } from "../util/composeMidiFile";
+import { flatten, curry, compose } from "ramda";
+import { playMidi } from "../util/playMidi";
 
-const handleSubmit = curry((event, props) => {
-    event.preventDefault();
-    const allChords = document.getElementById("chordInput").value
-    props.setChords(allChords)
-    const chords = flatten(parseSong(allChords, 4));
-    const midiDataUri = composeMidiFile(chords, 240)
-    playMidi(midiDataUri);
-})
- 
-const ChordInput = props => (
+const handleSubmit = (event, setChords) => {
+  event.preventDefault();
+  const allChords = document.getElementById("chordInput").value;
+  setChords(allChords);
+  const chords = flatten(parseSong(allChords, 4));
+  const midiDataUri = composeMidiFile(chords, 240);
+  playMidi(midiDataUri);
+};
+
+const ChordInput = props => {
+  const [chords, setChords] = useState("");
+  return (
     <div>
-      <form onSubmit={handleSubmit(props)}>
-      <textarea id="chordInput" />
-        <input type="submit" value="Play"/>
+      <form onSubmit={e => handleSubmit(e, setChords)}>
+        <textarea
+          id="chordInput"
+          value={chords}
+          onChange={e => setChords(e.target.value)}
+        />
+        <input type="submit" value="Play" />
       </form>
-      </div>
-)
+    </div>
+  );
+};
 
-export default compose(
-    withState('chords', 'setChords', "")
-)(ChordInput);
+export default ChordInput;
