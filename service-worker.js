a622172212afa6d5e3a65f59a169d9283c34657c
jottenlips <jottenlips@gmail.com>
2020-02-22 21:47:30 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/making-midi/precache-manifest.b71a9ce68da09fa7450ca9313196c11d.js"
+  "/making-midi/precache-manifest.bd763bf11283c939fb291c285f57826e.js"
 );
 
 workbox.clientsClaim();
